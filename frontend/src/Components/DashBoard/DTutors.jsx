09dc2ab94ae8d3659ae7776e6259c2bf49867246
Rupa Anu Priya Pulagam <rupaanupriyapulagam@gmail.com>
2025-06-19 16:14:54 +0530

    Update DTutors.jsx

--- frontend/src/Components/DashBoard/DTutors.jsx
+++ frontend/src/Components/DashBoard/DTutors.jsx
@@ -7,7 +7,7 @@ import Navbar from './Navbar';
 function Tutors() {
   const [tutors , setTutors] = useState([]);
   useEffect(()=>{
-   fetch("https://inspiring-balance-production.up.railway.app/tutors").then((data)=>data.json()).then((res)=>setTutors(res));
+   fetch("https://backend-production-eff3.up.railway.app/tutors").then((data)=>data.json()).then((res)=>setTutors(res));
   },[])
   console.log(tutors);
   return (

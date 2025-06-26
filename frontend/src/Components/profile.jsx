8374ae044a64ea2711d73bf552cae5e748f223ee
Rupa Anu Priya Pulagam <rupaanupriyapulagam@gmail.com>
2025-06-26 10:06:06 +0530

    Update profile.jsx

--- frontend/src/Components/profile.jsx
+++ frontend/src/Components/profile.jsx
@@ -59,75 +59,77 @@ function Profile() {
   return (
     <div style={{ background: "linear-gradient(to top, rgb(240, 161, 23),rgb(127, 192, 242)" }}>
       <Navbar page={"profile"} />
-      <div className="profile-card" id="pbg" style={{ marginTop:'100px', background: "linear-gradient(to right, rgb(252, 234, 203),rgb(199, 230, 255)" }}>
-        <ImgUpload onChange={handleImageChange} src={profileImage} />
-        <h2 className="profile-name">{userDetails?.username}</h2>
-        <div style={{ marginTop: '20px' }}>
-          <h4>Email: </h4>
-          <p className="profile-email">{userDetails?.email}</p>
-        </div>
-        <div>
-          <h4>Phone Number: </h4>
-          <p className="profile-phno">{userDetails?.phno}</p>
-        </div>
-        <div>
-        <h4>Gender: </h4>
-        <p className="profile-gender">{userDetails?.gender}</p>
-      </div>
-      <div>
-        <h4>Date of Birth: </h4>
-        <p className="profile-dob">{userDetails?.dob}</p>
-      </div>
-      <div>
-        <h4>Profession: </h4>
-        <p className="profile-gender">{userDetails?.profession}</p>
-      </div>
-        <div>
-          <h4>Learning courses: </h4>
-          <p className="profile-phno">{userDetails?.learningCourses.length}</p>
-        </div>
-        <div
-          style={{
-            marginTop: '10px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <a
-            href={userDetails?.linkedin_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              marginRight: '15px',
-              color: '#0077B5',
-              textDecoration: 'none',
-              display: 'flex',
-              alignItems: 'center',
-              transition: 'color 0.3s ease',
-            }}
-            onMouseOver={(e) => e.target.style.color = '#004471'}
-            onMouseOut={(e) => e.target.style.color = '#0077B5'}
-          >
-            <FontAwesomeIcon icon={faLinkedin} className="social-icon" style={{ fontSize: '38px' }} />
-          </a>
-          <a
-            href={userDetails?.github_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              color: 'darkviolet',
-              textDecoration: 'none',
-              display: 'flex',
-              alignItems: 'center',
-              transition: 'color 0.3s ease',
-            }}
-            onMouseOver={(e) => e.target.style.color = '#000'}
-            onMouseOut={(e) => e.target.style.color = 'darkviolet'}
-          >
-            <FontAwesomeIcon icon={faGithub} className="social-icon" style={{ fontSize: '38px' }} />
-          </a>
-        </div>
+      <div style={{paddingTop:"90px"}}>
+            <div className="profile-card" id="pbg" style={{ marginTop:'100px', background: "linear-gradient(to right, rgb(252, 234, 203),rgb(199, 230, 255)" }}>
+              <ImgUpload onChange={handleImageChange} src={profileImage} />
+              <h2 className="profile-name">{userDetails?.username}</h2>
+              <div style={{ marginTop: '20px' }}>
+                <h4>Email: </h4>
+                <p className="profile-email">{userDetails?.email}</p>
+              </div>
+              <div>
+                <h4>Phone Number: </h4>
+                <p className="profile-phno">{userDetails?.phno}</p>
+              </div>
+              <div>
+              <h4>Gender: </h4>
+              <p className="profile-gender">{userDetails?.gender}</p>
+            </div>
+            <div>
+              <h4>Date of Birth: </h4>
+              <p className="profile-dob">{userDetails?.dob}</p>
+            </div>
+            <div>
+              <h4>Profession: </h4>
+              <p className="profile-gender">{userDetails?.profession}</p>
+            </div>
+              <div>
+                <h4>Learning courses: </h4>
+                <p className="profile-phno">{userDetails?.learningCourses.length}</p>
+              </div>
+              <div
+                style={{
+                  marginTop: '10px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <a
+                  href={userDetails?.linkedin_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    marginRight: '15px',
+                    color: '#0077B5',
+                    textDecoration: 'none',
+                    display: 'flex',
+                    alignItems: 'center',
+                    transition: 'color 0.3s ease',
+                  }}
+                  onMouseOver={(e) => e.target.style.color = '#004471'}
+                  onMouseOut={(e) => e.target.style.color = '#0077B5'}
+                >
+                  <FontAwesomeIcon icon={faLinkedin} className="social-icon" style={{ fontSize: '38px' }} />
+                </a>
+                <a
+                  href={userDetails?.github_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    color: 'darkviolet',
+                    textDecoration: 'none',
+                    display: 'flex',
+                    alignItems: 'center',
+                    transition: 'color 0.3s ease',
+                  }}
+                  onMouseOver={(e) => e.target.style.color = '#000'}
+                  onMouseOut={(e) => e.target.style.color = 'darkviolet'}
+                >
+                  <FontAwesomeIcon icon={faGithub} className="social-icon" style={{ fontSize: '38px' }} />
+                </a>
+              </div>
+         </div>
       </div>
       <Performance />
     </div>
